cb4dbdacdaf5a4e52b0e136326c9c1f38ff81496
Bhagyadeep <b.ghosh0330@gmail.com>
2024-02-02 23:07:19 +0530

    feature added

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const input = document.querySelector(".text input");
 const lists = document.querySelector(".lists ul");
 
-let list = document.createElement("li");
-let listSpan = document.createElement("span");
+
+
 
 const submit = () => {
-    if (input.value === ""){
+    if (input.value === "") {
         alert("To-Do list cannot be empty");
     }
-    else{
+    else {
+        // label tag created
+        let listLabel = document.createElement("label");
+
+        //list tag created and added to label tag 
+        let list = document.createElement("li");
         list.innerText = input.value;
-        lists.appendChild(list);
+        list.classList.add("uncheck")
+        listLabel.appendChild(list);
         
+        // sapn tag created and added to label tag
+        let listSpan = document.createElement("span");
         listSpan.innerText = "X";
-        list.appendChild(listSpan);
+        listLabel.appendChild(listSpan);
+        
+        // label tag created and added to ul tag
+        lists.appendChild(listLabel);
+        
+        // check and uncheck function
+        list.addEventListener("click", () => {
+            list.classList.toggle("uncheck");
+            list.classList.toggle("check");
+            console.log("class is toggled");
+        })
+        // delete function
+        listSpan.onclick = () => {
+            lists.removeChild(listLabel);
+        }
     }
     input.value = "";
 }
 
-listSpan.addEventListener("click",() =>{
-    lists.removeChild(list);
-})
+
+
